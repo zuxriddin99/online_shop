4e71e8213e58a81d3e4e9db3fb8472f01a969148
acer <zux.828@gmail.com>
2022-12-04 16:51:08 +0300

    add static files

--- apps/orders/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from apps.orders.models import Order
-
-
-# class OrderForm(forms.ModelForm):
-#     class Meta:
-#         model = Order
-#         fields = ('user', 'city', 'address', 'country', 'default')
